32ffdf931be287c51bc3a6beb9c64556ab8e7ba4
Rapuska <mikalajsiatkouski@gmail.com>
2024-05-01 01:17:52 +0300

    hide all ui on start

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 use crate::*;
+use crate::gameplay::hud::components::GameplayHUD;
+use crate::main_menu::MainMenu;
 use crate::main_menu::systems::*;
-use crate::ui::systems::build_loading_curtain;
+use crate::ui::systems::{build_loading_curtain, hide};
 
 use self::gameplay::hud::systems::*;
 use self::systems::*;
@@ -19,7 +21,9 @@ impl Plugin for BootstrapPlugin {
                 build_gameplay_hud,
                 spawn_camera,
                 spawn_light,
-            ))
+                hide::<GameplayHUD>,
+                hide::<MainMenu>,
+            ).chain())
 
             .add_systems(PostUpdate, open_main_menu.run_if(in_state(AppState::Bootstrap)))
         ;
